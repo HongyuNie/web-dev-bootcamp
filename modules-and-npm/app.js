0977c0cbe402b81f9c18ac1ea4b6c698ae9d3aa9
HongyuNie <nhy0613@outlook.com>
2022-10-13 10:15:57 -0700

    require directory shelter in app.js

--- modules-and-npm/app.js
+++ modules-and-npm/app.js
@@ -1,9 +1,9 @@
 // const math = require('./math');
 const { PI, square } = require('./math');
 
-// const cats = require('./shelter')
+const cats = require('./shelter')
 // console.log(math.PI)
 // console.log(math.square(9))
-console.log(PI)
-console.log(square(9))
-// console.log("REQUIRED AN ENTIRE DIRECTORY!", cats)
+// console.log(PI)
+// console.log(square(9))
+console.log("REQUIRED AN ENTIRE DIRECTORY!", cats)
