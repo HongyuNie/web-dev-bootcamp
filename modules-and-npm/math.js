bc84100b06497803679c66730e7d11094e8c7b4b
HongyuNie <nhy0613@outlook.com>
2022-10-13 08:12:18 -0700

    module.exports shortcut syntax

--- modules-and-npm/math.js
+++ modules-and-npm/math.js
@@ -1,11 +1,11 @@
-module.exports.add = (x, y) => x + y;
+const add = (x, y) => x + y;
 
-module.exports.PI = 3.14159;
+const PI = 3.14159;
 
-module.exports.square = x => x * x;
+const square = x => x * x;
 
-// exports.square = square;
-// exports.PI = PI;
+exports.square = square;
+exports.PI = PI;
 
 // =========================
 // module.exports = "HELLLOOO";
