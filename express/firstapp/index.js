949a29a21439a652a48c85516244768e34a4456a
HongyuNie <nhy0613@outlook.com>
2022-10-14 16:56:47 -0700

    pattern matching with extracting the name from params and repond accordingly

--- express/firstapp/index.js
+++ express/firstapp/index.js
@@ -30,8 +30,16 @@ app.get('/', (req, res) => {
     res.send('WELCOME TO ANIMALS SOUNDS PAGE!')
 })
 
+//pattern match
+app.get('/r/:subreddit', (req, res) => {
+    // console.log(req.params)
+    const { subreddit } = req.params
+    res.send(`<h1>Browsing the ${subreddit} subreddit </h1>`)
+    // res.send('SUBREDDIT ALERT!!!')
+})
 
-//routes are matched in order, if put this in above, /cats or /dogs will both be unknown.
+
+//routes are matched in order, if put this in above, /cats or /dogs will both be unknown. * means matchs any single request
 app.get('*', (req, res) => { 
     res.send('IDK...')
 })
