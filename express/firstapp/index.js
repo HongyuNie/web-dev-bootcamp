b343e9e6dd1ca00458387bb60766d1265815ce40
HongyuNie <nhy0613@outlook.com>
2022-10-14 15:36:37 -0700

    basic callback pattern for responding on root and dogs request

--- express/firstapp/index.js
+++ express/firstapp/index.js
@@ -8,8 +8,19 @@ app.listen(8080, () => {
     console.log("LISTENING ON PORT 8080!")
 })
 
+
+
 //request is an object created by express based upon the incoming HTTP request
 app.get('/cats', (req, res) => { 
     // console.log("CAT REQUEST!!")
     res.send('MEOOOOWW!')
+})
+
+app.get('/dogs', (req, res) => { 
+    // console.log("DOG REQUEST!!")
+    res.send('WOOOOOF!')
+})
+
+app.get('/', (req, res) => { 
+    res.send('WELCOME TO ANIMALS SOUNDS PAGE!')
 })
