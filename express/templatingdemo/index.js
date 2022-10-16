29589e6ac0cf36c80b2d91aea64a0aaf40e10ecd
HongyuNie <nhy0613@outlook.com>
2022-10-16 13:48:02 -0700

    path setting for running outside the correct folder

--- express/templatingdemo/index.js
+++ express/templatingdemo/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('view engine', 'ejs') //a key value pair
 
+app.set('views', path.join(__dirname, '/views'));//using the directory name where index.js is located. allow you to run outside the correct folder
+    
 app.get('/', (req, res) => { 
     res.render('home.ejs');
 })
