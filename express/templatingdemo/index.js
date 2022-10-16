bc2f16f05ec9bb1cd178fe0d1fc15cb44402d00a
HongyuNie <nhy0613@outlook.com>
2022-10-16 16:19:27 -0700

    install ejs extension, try out the tag of outouts the value to the template

--- express/templatingdemo/index.js
+++ express/templatingdemo/index.js
@@ -9,7 +9,7 @@ app.set('views', path.join(__dirname, '/views'));//using the directory name wher
 app.get('/', (req, res) => { 
     res.render('home.ejs');
 })
-
+ 
 app.listen(3000, () => { 
     console.log("LISTENING ON PORT 3000")
 })
