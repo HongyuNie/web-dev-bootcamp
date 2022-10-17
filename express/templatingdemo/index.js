549fa9ec3e71b89e070df6e508d44611c3cbe940
HongyuNie <nhy0613@outlook.com>
2022-10-16 23:00:57 -0700

    pass a generated random number to the template using variable rand

--- express/templatingdemo/index.js
+++ express/templatingdemo/index.js
@@ -1,3 +1,4 @@
+const { resolveSoa } = require('dns');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -7,9 +8,14 @@ app.set('view engine', 'ejs') //a key value pair
 app.set('views', path.join(__dirname, '/views'));//using the directory name where index.js is located. allow you to run outside the correct folder
     
 app.get('/', (req, res) => { 
-    res.render('home.ejs');
+    res.render('home');
 })
  
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10 + 1);
+    res.render('random', {rand: num});
+})
+
 app.listen(3000, () => { 
     console.log("LISTENING ON PORT 3000")
 })
