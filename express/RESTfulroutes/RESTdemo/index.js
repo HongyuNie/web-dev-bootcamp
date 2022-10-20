b3ce006aab707a951fdd166f3a20b877d8ec4d83
HongyuNie <nhy0613@outlook.com>
2022-10-20 16:39:25 -0700

    parse the request body using middleware as urlencoded/json payloads.

--- express/RESTfulroutes/RESTdemo/index.js
+++ express/RESTfulroutes/RESTdemo/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 
+// running on every single request
+// use this middleware to parse the req.body as url encoded data
+app.use(express.urlencoded({ extended: true }));
+// for parsing incoming json payloads
+app.use(express.json());
+
 app.get('/tacos', (req, res) => { 
-    res.send("GET /tacos response")
+    res.send("GET /tacos response");
 }) 
 
-app.post('/tacos', (req, res) => { 
-    res.send("POST /tacos response")
+app.post('/tacos', (req, res) => {
+    // console.log(req.body);
+    const { meat, qty } = req.body;
+    res.send(`OK, here are you ${qty} ${meat} tacos!!`);
 })
 app.listen(3000, () => {
     console.log("ON PORT 3000")
