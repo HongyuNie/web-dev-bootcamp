f29072c6da3840071453ec979d3607cc688a9a82
HongyuNie <nhy0613@outlook.com>
2022-10-20 21:00:58 -0700

    set up comments index route

--- express/RESTfulroutes/RESTdemo/index.js
+++ express/RESTfulroutes/RESTdemo/index.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // running on every single request
 // use this middleware to parse the req.body as url encoded data
 app.use(express.urlencoded({ extended: true }));
 // for parsing incoming json payloads
 app.use(express.json());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const comments = [
+    {
+        // id: uuid(),
+        username: 'Todd',
+        comment: 'lol that is so funny!'
+    },
+    {
+        // id: uuid(),
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {
+        // id: uuid(),
+        username: 'Sk8erBoi',
+        comment: 'Plz delete your account, Todd'
+    },
+    {
+        // id: uuid(),
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+    }
+]
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index', {comments});
+})
+
 
 app.get('/tacos', (req, res) => { 
     res.send("GET /tacos response");
