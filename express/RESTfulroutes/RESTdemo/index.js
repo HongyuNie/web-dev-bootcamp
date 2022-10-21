8e6fa3f06f9383e17ff17ac33c6059c9331619e4
HongyuNie <nhy0613@outlook.com>
2022-10-21 11:24:55 -0700

    include uuid package generating id number for new comment

--- express/RESTfulroutes/RESTdemo/index.js
+++ express/RESTfulroutes/RESTdemo/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const { v4: uuid } = require('uuid');
+uuid();
+
 // running on every single request
 // use this middleware to parse the req.body as url encoded data
 app.use(express.urlencoded({ extended: true }));
@@ -12,22 +15,21 @@ app.set('view engine', 'ejs');
 
 const comments = [
     {
-        id: 1,
+        id: uuid(),
         username: 'Todd',
         comment: 'lol that is so funny!'
     },
     {
-        id: 2,
+        id: uuid(),
         username: 'Skyler',
         comment: 'I like to go birdwatching with my dog'
     },
-    {
-        id: 3,
+    {   id: uuid(),
         username: 'Sk8erBoi',
         comment: 'Plz delete your account, Todd'
     },
     {
-        id: 4,
+        id: uuid(),
         username: 'onlysayswoof',
         comment: 'woof woof woof'
     }
@@ -45,13 +47,13 @@ app.get('/comments/new', (req, res) => {
 app.post('/comments', (req, res) => { 
     // console.log(req.body);
     const { username, comment } = req.body;
-    comments.push({ username, comment });
+    comments.push({ username, comment, id: uuid()});
     res.redirect('/comments');
 })
 
 app.get('/comments/:id', (req, res) => {
     const { id } = req.params;
-    const comment = comments.find(c => c.id === parseInt(id));
+    const comment = comments.find(c => c.id === id);
     res.render('comments/show', {comment});
 })
 
