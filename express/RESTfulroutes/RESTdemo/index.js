14a4361bbebe047999b42a8b4e8747a436fb205e
HongyuNie <nhy0613@outlook.com>
2022-10-22 23:50:15 -0700

    use express method overreide patch for achieve patch method

--- express/RESTfulroutes/RESTdemo/index.js
+++ express/RESTfulroutes/RESTdemo/index.js
@@ -1,19 +1,23 @@
+
+const path = require('path');
+const methodOverride = require('method-override')
+const { v4: uuid } = require('uuid'); //For generating ID's
 const express = require('express');
 const app = express();
-const path = require('path');
 
-const { v4: uuid } = require('uuid');
-uuid();
 
-// running on every single request
-// use this middleware to parse the req.body as url encoded data
-app.use(express.urlencoded({ extended: true }));
-// for parsing incoming json payloads
-app.use(express.json());
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+//To parse form data in POST request body:
+app.use(express.urlencoded({ extended: true }))
+// To parse incoming JSON in POST request body:
+app.use(express.json())
+// To 'fake' put/patch/delete requests:
+app.use(methodOverride('_method'))
+// Views folder and EJS setup:
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-const comments = [
+// Our fake database:
+let comments = [
     {
         id: uuid(),
         username: 'Todd',
@@ -24,7 +28,8 @@ const comments = [
         username: 'Skyler',
         comment: 'I like to go birdwatching with my dog'
     },
-    {   id: uuid(),
+    {
+        id: uuid(),
         username: 'Sk8erBoi',
         comment: 'Plz delete your account, Todd'
     },
@@ -34,47 +39,204 @@ const comments = [
         comment: 'woof woof woof'
     }
 ]
-
-
+// **********************************
+// INDEX - renders multiple comments
+// **********************************
 app.get('/comments', (req, res) => {
-    res.render('comments/index', {comments});
+    res.render('comments/index', { comments });
 })
-
+// **********************************
+// NEW - renders a form
+// **********************************
 app.get('/comments/new', (req, res) => {
-    res.render('comments/new', {comments});
+    res.render('comments/new');
 })
-
-app.post('/comments', (req, res) => { 
-    // console.log(req.body);
+// **********************************
+// CREATE - creates a new comment
+// **********************************
+app.post('/comments', (req, res) => {
     const { username, comment } = req.body;
-    comments.push({ username, comment, id: uuid()});
+    comments.push({ username, comment, id: uuid() })
     res.redirect('/comments');
 })
-
+// *******************************************
+// SHOW - details about one particular comment
+// *******************************************
 app.get('/comments/:id', (req, res) => {
     const { id } = req.params;
     const comment = comments.find(c => c.id === id);
-    res.render('comments/show', {comment});
+    res.render('comments/show', { comment })
 })
-
-//for updating a existing comment, only revising comments content without user name and id
+// *******************************************
+// EDIT - renders a form to edit a comment
+// *******************************************
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', { comment })
+})
+// *******************************************
+// UPDATE - updates a particular comment
+// *******************************************
 app.patch('/comments/:id', (req, res) => {
     const { id } = req.params;
-    const newCommentText = req.body.comment;
     const foundComment = comments.find(c => c.id === id);
-    foundComment = newCommentText;
+
+    //get new text from req.body
+    const newCommentText = req.body.comment;
+    //update the comment with the data from req.body:
+    foundComment.comment = newCommentText;
+    //redirect back to index (or wherever you want)
+    res.redirect('/comments')
+})
+
+// *******************************************
+// DELETE/DESTROY- removes a single comment
+// *******************************************
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    comments = comments.filter(c => c.id !== id);
     res.redirect('/comments');
 })
 
-app.get('/tacos', (req, res) => { 
-    res.send("GET /tacos response");
-}) 
+app.get('/tacos', (req, res) => {
+    res.send("GET /tacos response")
+})
 
 app.post('/tacos', (req, res) => {
-    // console.log(req.body);
     const { meat, qty } = req.body;
-    res.send(`OK, here are you ${qty} ${meat} tacos!!`);
+    res.send(`OK, here are your ${qty} ${meat} tacos`)
 })
+
 app.listen(3000, () => {
-    console.log("ON PORT 3000")
+    console.log("ON PORT 3000!")
 })
+
+
+// GET /comments - list all comments
+// POST /comments - Create a new comment 
+// GET /comments/:id - Get one comment (using ID)
+// PATCH /comments/:id - Update one comment
+// DELETE /comments/:id - Destroy one comment
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// const methodOverride = require('method-override');
+
+// const { v4: uuid } = require('uuid');
+// uuid();
+
+
+
+// // running on every single request
+// // use this middleware to parse the req.body as url encoded data
+// app.use(express.urlencoded({ extended: true }));
+// // for parsing incoming json payloads
+// app.use(express.json());
+// app.use(methodOverride('_method'));
+
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+
+// const comments = [
+//     {
+//         id: uuid(),
+//         username: 'Todd',
+//         comment: 'lol that is so funny!'
+//     },
+//     {
+//         id: uuid(),
+//         username: 'Skyler',
+//         comment: 'I like to go birdwatching with my dog'
+//     },
+//     {   id: uuid(),
+//         username: 'Sk8erBoi',
+//         comment: 'Plz delete your account, Todd'
+//     },
+//     {
+//         id: uuid(),
+//         username: 'onlysayswoof',
+//         comment: 'woof woof woof'
+//     }
+// ]
+
+
+// app.get('/comments', (req, res) => {
+//     res.render('comments/index', {comments});
+// })
+
+// app.get('/comments/new', (req, res) => {
+//     res.render('comments/new', {comments});
+// })
+
+// app.post('/comments', (req, res) => {
+//     // console.log(req.body);
+//     const { username, comment } = req.body;
+//     comments.push({ username, comment, id: uuid()});
+//     res.redirect('/comments');
+// })
+
+// app.get('/comments/:id', (req, res) => {
+//     const { id } = req.params;
+//     const comment = comments.find(c => c.id === id);
+//     res.render('comments/show', {comment});
+// })
+
+// app.get('/comments/:id/edit', (req, res) => {
+//     const { id } = req.params;
+//     const comment = comments.find(c => c.id === id);
+//     res.render('comments/edit', {comment});
+// })
+
+// //for updating a existing comment, only revising comments content without user name and id
+// app.patch('/comments/:id', (req, res) => {
+//     const { id } = req.params;
+//     const newCommentText = req.body.comment;
+//     const foundComment = comments.find(c => c.id === id);
+//     foundComment = newCommentText;
+//     res.redirect('/comments');
+// })
+
+
+// app.get('/tacos', (req, res) => {
+//     res.send("GET /tacos response");
+// })
+
+// app.post('/tacos', (req, res) => {
+//     // console.log(req.body);
+//     const { meat, qty } = req.body;
+//     res.send(`OK, here are you ${qty} ${meat} tacos!!`);
+// })
+// app.listen(3000, () => {
+//     console.log("ON PORT 3000")
+// })
+
+
+
+
